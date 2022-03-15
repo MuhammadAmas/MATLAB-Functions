e612dc95075fe388e7bc90d7317f6083c67299e2
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-03-16 00:12:24 +0500

    Merge branch 'master' of https://github.com/MuhammadAmas/MATLAB-Functions

---
+++ Median.m
@@ -0,0 +1,20 @@
+function [median] = Median(L, U, F)
+sum_f=sum(F);
+average=sum_f/2;
+cf=cumsum(F);
+j=cf(1);
+i=1;
+while (j<average)
+    if(cf(i)>average)
+        j=cf(i);
+        m=i;
+    end
+    i=i+1;
+end
+l=L(m);
+cfp=cf(m-1);
+f=F(m);
+C=U(1)-L(1);
+median=l+((average-cfp)/f)*C;
+end
+
